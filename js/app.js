c08e06910af416a78900d0e2c2ba4792661a83db
Natalia Jaguś <natalia.jagus@gmail.com>
2018-08-30 20:09:41 +0200

    Added ajax and css grid

--- js/app.js
+++ js/app.js
@@ -6,8 +6,41 @@ $( document ).ready(function() {
     const button = document.createElement("button");
     button.innerHTML = "Random quote";
 
+    let quoteArea = document.querySelector("#quote");
+    let quoteName = document.querySelector("#name");
+
+    console.log(quoteArea);
+
     $('.quote').append(button);
 
+    $('.logo').hide();
+    $('.logo').slideDown(500);
+    
+    $("#quote").hide();
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+
+    	if (xhr.readyState === 4 && xhr.status === 200) {
+    		let quotes = JSON.parse(xhr.responseText);
+
+    		button.onclick = function() {
+                var randomNumber = Math.floor(Math.random() * (quotes.length));
+    			quoteArea.innerText = '„' + quotes[randomNumber].quote + '"' + ' - ' + quotes[randomNumber].name;
+
+              
+                $("#quote").slideDown(500).delay(5000);
+
+    		}
+
+    	}
+
+    }
+
+    xhr.open('GET', 'js/quotes.json');
+
+    xhr.send();
 
 
 });
